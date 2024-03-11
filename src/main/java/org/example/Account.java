9ec470f404957e16c9ccf66c15523c768089be19
Mmln <aammln00@gmail.com>
2024-03-11 21:50:03 +0500

    part 2 apply command for Undo

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -2,16 +2,25 @@ package org.example;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 
 public class Account {
     private String name;
-    private HashMap<CurTypes, BigDecimal> currency;
+    private HashMap<CurTypes, BigDecimal> currencies;
+    private Deque<Command> commands = new ArrayDeque<>();
 
     private Account(){}
     public Account(String name) {
         this.setName(name);
-        this.currency = new HashMap<>();
+        this.currencies = new HashMap<>();
+    }
+
+    public Account undo() throws NothingToUndo{
+        if (commands.isEmpty()) throw new NothingToUndo();
+        commands.pop().execute();
+        return this;
     }
 
     public String getName() {
@@ -20,21 +29,29 @@ public class Account {
 
     public void setName(String name) {
         if (name == null || name.isEmpty()) throw new IllegalArgumentException("The Account.name must not be empty");
+        String oldName = this.name;
+        this.commands.push(()->{this.name = oldName;});
         this.name = name;
     }
 
-    public HashMap<CurTypes, BigDecimal> getCurrency() {
-        return new HashMap<CurTypes, BigDecimal>(this.currency);
+    public HashMap<CurTypes, BigDecimal> getcurrencies() {
+        return new HashMap<CurTypes, BigDecimal>(this.currencies);
     }
 
-    public void putCurrency(CurTypes curtype, BigDecimal val) {
-        if (val.signum() != 1) throw new IllegalArgumentException("The currency value must be greater then zero");
-        this.currency.put(curtype, val);
+    public void putCurrencies(CurTypes curtype, BigDecimal val) {
+        if (val.signum() != 1) throw new IllegalArgumentException("The currencies value must be greater then zero");
+        if (currencies.containsKey(curtype)){
+            BigDecimal oldVal = this.currencies.get(curtype);
+            this.commands.push(()->{this.currencies.put(curtype,oldVal);});
+        } else {
+            this.commands.push(()->{this.currencies.remove(curtype);});
+        }
+        this.currencies.put(curtype, val);
     }
 
     @Override
     public String toString() {
-        return "Account{" + "name='" + name + '\'' + ", currency=" + currency + '}';
+        return "Account{" + "name='" + name + '\'' + ", currencies=" + currencies + '}';
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,28 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println("MmlnTask01 started...");
+        System.out.println("MmlnTask_01 started...");
 
         //Account acc1 = new Account(); not compiled
         Account acc = new Account("Andy");
         //acc.putCurrency(CurTypes.RUB, BigDecimal.valueOf(-100)); -- throw the error
         //System.out.println(acc);
-        acc.putCurrency(CurTypes.RUB, BigDecimal.valueOf(100));
+        acc.putCurrencies(CurTypes.RUB, BigDecimal.valueOf(100));
         System.out.println(acc);
-        acc.putCurrency(CurTypes.RUB, BigDecimal.valueOf(200));
+        acc.putCurrencies(CurTypes.RUB, BigDecimal.valueOf(200));
         System.out.println(acc);
-        acc.putCurrency(CurTypes.USD, BigDecimal.valueOf(50));
+        acc.putCurrencies(CurTypes.USD, BigDecimal.valueOf(50));
         System.out.println(acc);
 
-        System.out.println("MmlnTask01 finished...");
+        acc.undo();
+        System.out.println(acc);
+        acc.undo();
+        System.out.println(acc);
+        acc.undo();
+        System.out.println(acc);
+        acc.undo();
+        System.out.println(acc);
+
+        System.out.println("MmlnTask_01 finished...");
     }
 }
